934f2ba6e44a061fb0e0368151e5ded18bf84d0f
iaconom821 <iaconom821@gmail.com>
2021-04-07 15:59:54 -0400

    sort descending by username

--- src/components/CommentCard.js
+++ src/components/CommentCard.js
@@ -3,6 +3,7 @@ import React, { useState } from "react"
 function CommentCard ({comment, user}) {
     const [upvotesValue, setUpvotesValue] = useState(0)
     const [downvotesValue, setDownvotesValue] = useState(0)
+    const [hidden, setHidden] = useState(false)
 
     function upvoteClickHandle(event) {
         setUpvotesValue(upvotesValue + 1)
@@ -12,12 +13,18 @@ function CommentCard ({comment, user}) {
         setDownvotesValue(downvotesValue + 1)
     }
 
+    function deleteComment(evt){
+        setHidden(!hidden)
+    }
+
     return (
-        <div className="video-comments">
+        <div hidden={ hidden } className="video-comments">
             <h3 className="user-name">{user}</h3>
             <p className="comment">{comment}</p>
             <br/>
             <button className="upvotes" onClick={upvoteClickHandle}>{ upvotesValue }👍</button><button className="downvotes" onClick={downvoteClickHandle}>{ downvotesValue }👎︎</button>
+            <br/>
+            <button className="delete" onClick={ deleteComment }>Delete Comment</button>
         </div>
     )
 }
--- src/components/Comments.js
+++ src/components/Comments.js
@@ -3,12 +3,33 @@ import CommentCard from "./CommentCard"
 
 function Comments ({ comments }) {
     const [searchText, setSearchText] = useState("")
+    const [sort, setSort] = useState("All")
 
     function handlesChange(event){
         setSearchText(event.target.value)
     }
 
-    const filteredComments = comments.filter(function(comment) {
+    function handleSelect(evt) {
+        setSort(evt.target.value)
+    }
+
+    const sortedComments = [...comments]
+
+    if(sort === "descending") {
+        sortedComments.sort((commenta, commentb) => {
+            let nameA = commenta.user.toUpperCase(); // ignore upper and lowercase
+            let nameB = commentb.user.toUpperCase(); // ignore upper and lowercase
+            if (nameA < nameB) {
+                return 1;
+            }
+            if (nameA > nameB) {
+                return -1;
+            }
+            return 0
+        })
+    }
+
+    const filteredComments = sortedComments.filter(function(comment) {
         return comment.user.includes(searchText)
     })
 
@@ -22,7 +43,13 @@ function Comments ({ comments }) {
         <div>
             <h2>{comments.length} Comments</h2>
             <input type="text" placeholder="Search" onChange={handlesChange}></input>
+            <select onChange={ handleSelect }>
+                <option value="All">No Sort</option>
+                <option value="ascending">Ascending</option>
+                <option value="descending">Descending</option>
+            </select>
             {commentCards}
+            
         </div>
     )
 }
